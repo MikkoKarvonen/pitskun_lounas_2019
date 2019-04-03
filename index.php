f9506c4e15ea6943a57061029dcd54b5e85aae57
MikkoKarvonen <mkarvonen34@gmail.com>
2019-04-03 21:40:30 +0300

    Put data in table

--- index.php
+++ index.php
@@ -7,36 +7,48 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    $today = new DateTime();
-    $weeks = 0;
-    $missed = 0;
+    <table>
+        <thead>
+            <tr>
+                <th></th>
+                <th>Lounas</th>
+                <th>Kasvis</th>
+                <th>Deli</th>
+                <th>Jälkiruoka</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $today = new DateTime();
+            $weeks = 0;
+            $missed = 0;
+            $days = ['Su', 'Ma', 'Ti', 'Ke', 'To', 'Pe'];
 
-    while($weeks < 4){
-        if ($today->format('N') < 6){
-            $url = 'https://www.sodexo.fi/ruokalistat/output/daily_json/27793/'.$today->format('Y/m/d').'/fi';
-            $result = file_get_contents($url);
-            $res = json_decode($result, true);
-            if (empty($res[courses])){
-                $missed++;
-            }else {
-                echo $today->format('Y/m/d').' '.$today->format( 'N' ).'<br>';
-                foreach($res[courses] as $r) {
-                    echo $r[category] .' '. $r[title_fi] .' '. $r[properties], '<br>';
+            while($weeks < 4){
+                if ($today->format('N') < 6){
+                    $url = 'https://www.sodexo.fi/ruokalistat/output/daily_json/27793/'.$today->format('Y/m/d').'/fi';
+                    $result = file_get_contents($url);
+                    $res = json_decode($result, true);
+                    if (empty($res[courses])){
+                        $missed++;
+                    }else {
+                        $dayContent = '<td>'.$days[$today->format('w')].'<br>'.$today->format('j.n.').'</td>';
+                        foreach($res[courses] as $r) {
+                            $dayContent .= '<td>'.$r[title_fi] .'<br>'. $r[properties].'</td>';
+                        }
+                        echo '<tr>'.$dayContent.'</tr>';
+                    }
+                    if ($today->format('N') == 5){
+                        $weeks++;
+                    }
+                    if ($missed >= 3){
+                        break;
+                    }
                 }
+                $today->modify( '+1 days' );
             }
-            if ($today->format('N') == 5){
-                $weeks++;
-            }
-            if ($missed >= 3){
-                break;
-            }
-        }
-        $today->modify( '+1 days' );
-    }
-
-    ?>
-    <pre>
-</pre>
+            ?>
+        </tbody>
+    </table>
 </body>
 </html>
