40ecbc0cb39b61eed9b11c37c09cf2aa48fc7cd4
MikkoKarvonen <mkarvonen34@gmail.com>
2019-04-03 22:10:13 +0300

    Edit title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Pitskun lounas 2019</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
